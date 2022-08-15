5b37b1a4bffc9c78b8c74d46895c09d1645a81d7
JiSeongHah <89916891+JiSeongHah@users.noreply.github.com>
2022-08-15 19:17:17 +0900

    Delete csv_writing.py

--- csv_writing.py
+++
@@ -1,9 +0,0 @@
-import csv
-
-def lst2csv(lst,dir,name):
-    with open(dir+name,'a',newline='') as f:
-        write = csv.writer(f)
-        write.writerows(lst)
-
-    print('converting test lst to csv file complete')
-    print(f'final file saved in {dir+name}')
